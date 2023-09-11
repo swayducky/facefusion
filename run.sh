04b91e97f9c9954df1e5845bcbcc75c76234dada
Sway Ducky <swayducky@gmail.com>
2023-09-11 00:02:41 -0700

    working for GPU

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ "$OSTYPE" == "msys" ]]; then
-    source .venv/Scripts/activate
+    VENV_BIN=.venv/Scripts
 else
-    source .venv/bin/activate	
+    VENV_BIN=.venv/bin
 fi
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(dirname $($VENV_BIN/python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))/lib"
+source $VENV_BIN/activate
+echo "YO... LD LIBRARY: $LD_LIBRARY_PATH"
 python run.py --execution-providers cuda
